df4d386f975067afc332981f5e59d6b05245d326
Michael Park <mcypark@gmail.com>
2016-07-05 13:30:04 +0200

    Initial commit.

---
+++ llvm-mesos.rb
@@ -0,0 +1,102 @@
+class LlvmMesos < Formula
+  desc "Mesos-specific LLVM tools (`clang-format` and `clang-tidy`)"
+
+  head do
+    url "http://llvm.org/git/llvm.git", :branch => "release_38"
+
+    resource "clang" do
+      url "https://github.com/mesos/clang.git", :branch => "mesos_38"
+    end
+
+    resource "clang-tools-extra" do
+      url "https://github.com/mesos/clang-tools-extra.git", :branch => "mesos_38"
+    end
+
+    resource "libcxx" do
+      url "http://llvm.org/git/libcxx.git", :branch => "release_38"
+    end
+  end
+
+  option :universal
+
+  depends_on "cmake" => :build
+
+  # version suffix
+  def ver
+    "mesos-3.8"
+  end
+
+  # LLVM installs its own standard library which confuses stdlib checking.
+  cxxstdlib_check :skip
+
+  # Apple's libstdc++ is too old to build LLVM
+  fails_with :gcc
+  fails_with :llvm
+
+  def install
+    # Apple's libstdc++ is too old to build LLVM
+    ENV.libcxx if ENV.compiler == :clang
+
+    (buildpath/"tools/clang").install resource("clang")
+    (buildpath/"tools/clang/tools/extra").install resource("clang-tools-extra")
+    (buildpath/"projects/libcxx").install resource("libcxx")
+
+    install_prefix = lib/"llvm-#{ver}"
+
+    args = %w[
+      -DLLVM_OPTIMIZED_TABLEGEN=On
+      -DLLVM_BUILD_LLVM_DYLIB=On
+    ]
+
+    if build.universal?
+      ENV.permit_arch_flags
+      args << "-DCMAKE_OSX_ARCHITECTURES=#{Hardware::CPU.universal_archs.as_cmake_arch_flags}"
+    end
+
+    mktemp do
+      system "cmake", "-G", "Unix Makefiles", buildpath, *(std_cmake_args + args)
+      system "cmake --build . --target clang-format"
+      system "cmake --build . --target clang-tidy"
+      (install_prefix/"bin").install "bin/clang-format"
+      (install_prefix/"bin").install "bin/clang-tidy"
+    end
+
+    (install_prefix/"bin").install "tools/clang/tools/clang-format/git-clang-format"
+    (install_prefix/"share/clang").install Dir["tools/clang/tools/clang-format/clang-format*"]
+    (install_prefix/"share/clang").install "tools/clang/tools/extra/clang-tidy/tool/clang-tidy-diff.py"
+    (install_prefix/"share/clang").install "tools/clang/tools/extra/clang-tidy/tool/run-clang-tidy.py"
+
+    Dir.glob(install_prefix/"bin/*") do |exec_path|
+      basename = File.basename(exec_path)
+      bin.install_symlink exec_path => "#{basename}-#{ver}"
+    end
+
+    Dir.glob(install_prefix/"share/clang/*") do |script|
+      basename = File.basename(script, ".*")
+      extname = File.extname(script)
+      case extname
+      when ".py"
+        if basename.include? "clang-format"
+          inreplace script, "binary = 'clang-format'", "binary = 'clang-format-#{ver}'"
+        elsif basename.include? "clang-tidy"
+          inreplace script, "default='clang-tidy'", "default='clang-tidy-#{ver}'"
+        end
+      when ".el"
+        inreplace script, "executable-find \"clang-format\"", "executable-find \"clang-format-#{ver}\""
+      when ".applescript"
+        inreplace script, "path/to/clang-format", "path/to/clang-format-#{ver}"
+      end
+      (share/"clang-#{ver}").install_symlink script => "#{basename}-#{ver}#{extname}"
+    end
+  end
+
+  test do
+    # NB: below C code is messily formatted on purpose.
+    (testpath/"test.c").write <<-EOS
+      int         main(char *args) { \n   \t printf("hello"); }
+    EOS
+
+    assert_equal "int main(char *args) { printf(\"hello\"); }\n",
+        shell_output("#{bin}/clang-format -style=Google test.c")
+  end
+end
